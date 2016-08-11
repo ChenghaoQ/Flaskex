4324ec71fd08f2afa448628b5c1b5f92b37a9e4c
Zhongyu Li <zli@localhost.localdomain>
2016-08-10 19:42:13 -0500

    responsive

--- Frozen-flask/frozenflask/app/views.py
+++ Frozen-flask/frozenflask/app/views.py
@@ -6,7 +6,10 @@ from app import app,articles
 
 
 #set the articlelist route
-@app.route('/posts')
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/posts.html')
 def posts():
 	#posts = [article for article in articles if 'date' in article.meta]
 	posts=[]
@@ -16,18 +19,17 @@ def posts():
 
 	#sort posts by date,descending
 
-	sorted_posts = sorted(posts,reverse = True,key = lambda page:page.meta['date'])
+	sorted_posts = sorted(posts,reverse = True,key = lambda page:page.meta['date'])#Because of key is date, so in .md file date cannot be write in wrong format like Date
 	#pages may related to template index.html
 	return render_template('posts.html',pages = sorted_posts)
 
 
 
 #where does path come from
-@app.route('/<path:path>/')
+@app.route('/po/<path:path>/')
 def page(path):
-	#path is the filename of a page, without the file extension
+	#path is the filename of a page,https://github.com/ChenghaoQ/ChenghaoQ.git without the file extension
 	#e.g."first-post
-	print(path)
 	article = articles.get_or_404(path)
 	#page= article may have problem !!!!!1
 	#page may related to the template referal
