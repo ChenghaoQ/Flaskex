00f65082b9f20afeb1b24dbf51efb1180fcc5876
ChenghaoQ <ch.jakobqian@gmail.com>
2016-08-29 13:44:49 -0500

    google analytics, color change

--- Frozen-flask/frozenflask/app/__init__.py
+++ Frozen-flask/frozenflask/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
+
+
 #init the flask
 app = Flask(__name__)
+
 #load the settings from .py file
 app.config.from_pyfile('settings.py')
 #initialize the article pages
@@ -11,4 +14,13 @@ articles = FlatPages(app) #FlatPages use 'app' as arguments
 freezer = Freezer(app)
 
 #after initialize import views
-from app import views
+#from app import views
+
+from .view.index import index
+from .view.postwall import postwall
+from .view.article import article
+
+#注册蓝图
+app.register_blueprint(index)
+app.register_blueprint(postwall)
+app.register_blueprint(article)
